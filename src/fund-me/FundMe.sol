6e2ff1f03119137961b6699a2654efe529bb3cde
raihanmd <mraihanf471@gmail.com>
2025-07-15 11:06:39 +0700

    learn: modifier, etc

--- src/fund-me/FundMe.sol
+++ src/fund-me/FundMe.sol
@@ -1,8 +1,67 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
-contract FundMe {
-    function fund() public payable {}
+import {PriceConsumer} from "./lib/PriceConsumer.sol";
 
-    function withdraw() public {}
+error Unauthorized();
+error Forbidden(string message);
+
+contract FundMe {
+    using PriceConsumer for uint256;
+
+    uint256 constant MINIMUM_USD = 5e18;
+
+    address immutable i_owner;
+    address[] funders;
+    mapping(address funder => uint256 amountFunded) public funderToAmountFunded;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(
+            // PriceConsumer.getConversionRate(msg.value) >= MINIMUM_USD, //! Before
+            msg.value.getConversionRate() >= MINIMUM_USD, //* After before, bisa di langsung pake
+            Forbidden("Minimal value worth 5 USD")
+        );
+
+        funders.push(msg.sender);
+        funderToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public _onlyOwner {
+        // revert() // * Immidiately stop the transaction
+        for (uint256 i = 0; i < funders.length; i++) {
+            funderToAmountFunded[funders[i]] = 0;
+            // * transfer
+            // payable(funders[i]).transfer(funderToAmountFunded[funders[i]]);
+            // * send
+            // bool success = payable(funders[i]).send(
+            //     funderToAmountFunded[funders[i]]
+            // );
+            // require(success, "Send failed");
+            //  * call (lower level stuff), params 2 is returned from func that called in `.call()` params
+            (
+                bool successCall, // * params 2: `bytes memory data`
+
+            ) = payable(funders[i]).call{
+                    value: funderToAmountFunded[funders[i]]
+                }("");
+            require(successCall, "Call failed");
+        }
+
+        funders = new address[](0);
+    }
+
+    // * Called when calldata to it is blank
+    receive() external payable {}
+
+    // * Called when calldata to it is not blank, then the function that point to be called is not defined on contract
+    fallback() external payable {}
+
+    modifier _onlyOwner() {
+        require(msg.sender == i_owner, Unauthorized());
+        _;
+    }
 }
---
+++ src/fund-me/lib/PriceConsumer.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConsumer {
+    address private constant ETH_USD_PAIR_ADDRESS =
+        0x694AA1769357215DE4FAC081bf1f309aDC325306;
+
+    function getPrice() internal view returns (uint256) {
+        (, int256 price, , , ) = AggregatorV3Interface(ETH_USD_PAIR_ADDRESS)
+            .latestRoundData();
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(
+        uint256 _ethAmount
+    ) internal view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        return (_ethAmount * ethPrice) / 1e18;
+    }
+}
