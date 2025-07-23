c9b3b34e4d27b8ebfad28f5fac4e8af18b6d4ab9
raihanmd <mraihanf471@gmail.com>
2025-07-23 14:11:36 +0700

    makefile

--- script/Interaction.s.sol
+++ script/Interaction.s.sol
@@ -14,16 +14,35 @@ contract Fund is Script {
             block.chainid
         );
 
-        vm.startBroadcast();
         fund(mostRecentlyDeployed);
-        vm.stopBroadcast();
     }
 
     function fund(address _fundMeAddr) public {
+        vm.startBroadcast();
         FundMe(payable(_fundMeAddr)).fund{value: ETH_TO_BE_SENT}();
+        vm.stopBroadcast();
 
         console.log("Funded %s", ETH_TO_BE_SENT);
     }
 }
 
-contract Withdraw is Script {}
+contract Withdraw is Script {
+    uint constant ETH_TO_BE_SENT = 0.1 ether;
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+
+        withdraw(mostRecentlyDeployed);
+    }
+
+    function withdraw(address _fundMeAddr) public {
+        vm.startBroadcast();
+        FundMe(payable(_fundMeAddr)).withdraw();
+        vm.stopBroadcast();
+
+        console.log("Funded %s", ETH_TO_BE_SENT);
+    }
+}
--- test/integration/FundMeTestIntegration.t.sol
+++ test/integration/FundMeTestIntegration.t.sol
@@ -5,7 +5,7 @@ import {stdError} from "forge-std/StdError.sol";
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../../src/fund-me/FundMe.sol";
 import {FundMeScript} from "../../script/FundMeScript.s.sol";
-import {Fund} from "../../script/Interaction.s.sol";
+import {Fund, Withdraw} from "../../script/Interaction.s.sol";
 
 contract FundMeTestIntegration is Test {
     FundMe public fundMe;
@@ -18,17 +18,25 @@ contract FundMeTestIntegration is Test {
 
     function setUp() public {
         fundMe = new FundMeScript().run();
-
-        vm.deal(USER, INITIAL_BALANCE);
     }
 
-    function test_UserCanFund() public {
+    function test_UserCanFundInteraction() public {
         Fund fund = new Fund();
 
-        hoax(address(USER), INITIAL_BALANCE);
         fund.fund(address(fundMe));
 
         address funder = fundMe.getFunder(0);
-        assertEq(funder, USER);
+        assertEq(funder, msg.sender);
+    }
+
+    function test_UserCanWithdrawInteraction() public {
+        Fund fund = new Fund();
+        Withdraw withdraw = new Withdraw();
+
+        fund.fund(address(fundMe));
+
+        withdraw.withdraw(address(fundMe));
+
+        assertEq(address(fundMe).balance, 0);
     }
 }
